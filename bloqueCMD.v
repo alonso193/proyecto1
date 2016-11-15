0aa9bfe0fd2cd667f9794befe85d802081eeabf1
alonso193 <alonso.espinoza193@gmail.com>
2016-11-14 23:58:54 -0600

    actualización de los bloques de command

--- bloqueCMD.v
+++ bloqueCMD.v
@@ -1,4 +1,41 @@
 //el siguiente módulo se encarga de la transferencia de comandos del sdHost a la sdCard y de la sdCard hacia el sdHost
-module CMD (input clk, reset);
+module CMD (input clk_host, reset_host, new_command, IOin_SD, clk_SD, input [31:0]cmd_argument, input [5:0]cmd_index, output IOout_SD, CMD_COMPLETE);
 
 endmodule // CMD
+
+module cmd_control (clk_host, reset_host, new_command, cmd_argument, cmd_index, strobe_in, cmd_in, response, CMD_COMPLETE, strobe_out, idle_out, cmd_out);
+    //señales provenientes del host
+    input clk_host, reset_host;
+    //señales provenientes de los registros
+    input new_command;
+    input [31:0]cmd_argument;
+    input  [5:0]cmd_index;
+    //señales provenientes de la capa fisica
+    input strobe_in;
+    input [135:0]cmd_in;
+    //salidas hacia los registros
+    output reg [127:0]response;
+    output reg CMD_COMPLETE;
+    //salidas hacia capa fisica
+    output reg strobe_out;
+    output reg idle_out;
+    output reg [39:0]cmd_out;
+    always @ ( * ) begin
+        if (reset_host == 1) begin
+            response = 0;
+            CMD_COMPLETE = 0;
+            strobe_out = 0;
+            idle_out = 0;
+            cmd_out = 0;
+        end else begin
+            if (new_command == 1) begin
+                strobe_out = 1;
+                
+            end else begin
+
+            end
+        end
+    end
+
+
+endmodule // cmd_control
--- cmd_PAD_card.v
+++ cmd_PAD_card.v
@@ -1,31 +1,17 @@
-module cmd_PAD_card (input OutIn_control, ENB_control, data_in_parallelToSerial_PAD, clk_SD, IOin_SD, output data_out_serialToParallel, IOout_SD);
-    //wire OutIn_control, ENB_control, data_in_parallelToSerial_PAD, clk_SD, IOin_SD;
-    //reg data_out_serialToParallel, IOout_SD;
-    FF_D flipflop1(clk_SD, data_in_parallelToSerial_PAD, outToBuffer);
-    FF_D flipflop2(clk_SD, IOout_SD, data_out_serialToParallel);
-    buffer_triestado buffer(outToBuffer, OutIn_control, IOout_SD);
+//modulo de PAD que se comunicará con la SD y con los serializadores y paralelizadores de la capa fisica del CMD
+module cmd_PAD_card (input ENB_control, data_in_parallelToSerial_PAD, clk_SD, IOin_SD, output data_out_serialToParallel, IOout_SD);
+    flipflopD f1(ENB_control, clk_SD, data_in_parallelToSerial_PAD, IOout_SD);
+    flipflopD f2(ENB_control, clk_SD, IOin_SD,data_out_serialToParallel);
 endmodule // cmd_PAD_card
 
-module control_PAD ();
-
-endmodule // control_PAD
-
-module FF_D (input clk, D, output Q);
-    wire clk, D;
+//flipflop de tipo D
+module flipflopD (input ENB, clk, D, output Q);
     reg Q;
-    always @ ( posedge clk) begin
-        Q = D;
-    end
-endmodule // FF_D
-
-module buffer_triestado (input Dato, ENB, output out);
-    wire Dato, ENB;
-    reg out;
-    always @ ( * ) begin
+    always @ ( posedge clk ) begin
         if (ENB == 1) begin
-            out = Dato;
+            Q = D;
         end else begin
-            out = 1'bz;
+            Q = 1'bz;
         end
     end
-endmodule // buffer_triestado
+endmodule // flipflopD
