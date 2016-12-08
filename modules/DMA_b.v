023ef8469bc091793216a010e76453c505e808cf
luismoramora94 <lmoramora94@gmail.com>
2016-12-08 01:43:55 -0600

    Update DMA_b.v

--- modules/DMA_b.v
+++ modules/DMA_b.v
@@ -1,39 +1,45 @@
 
-module DMA(
+module DMA(input reset,                 
+           input clk,
+           input END,
+          // input data_transfer, todavia no se si lo ocupo
+           input TFC,
+           input valid,
+           input cmd_reg_write,
+           input cmd_reg_read,
+           input continue,
+           input TRAN,
+           input STOP,
+           input empty,
+           input full,
+           input [63:0] data_address,
+           input [15:0] lenght,
+           input [5:0] Attribute,
+           output start_transfer,
+           output goto_address, 
+           output [31:0]data_mem,
+           output [31:0] data_fifo,
+           output writing_mem,
+           output reading_mem,
+           output reading_fifo,
+           output writing_fifo); 
+ 
+ parameter ST_STOP  = 2'b00, 
+           ST_FDS   = 2'b01,
+           ST_CADR  = 2'b10,
+           ST_TFR   = 2'b11; 
+ 
+ reg   [1:0]          state        ;// Seq part of the FSM
+ wire  [1:0]          next_state   ;// combo part of FSM
 
-clock      , // clock
-reset      , // Active high, syn reset
-req_0      , // Request 0
-req_1      , // Request 1
-gnt_0      , // Grant 0
-gnt_1
-);
- input   clock,reset,req_0,req_1;
- output  gnt_0,gnt_1;
- wire    clock,reset,req_0,req_1;
- reg     gnt_0,gnt_1;
- parameter SIZE = 3
-         ;
- parameter ST_STOP  = 2'b00,ST_FDS = 2'b01,ST_CADR = 2'b10, ST_TFR = 2'b11;
-
- reg   [SIZE-1:0]          state        ;// Seq part of the FSM
- wire  [SIZE-1:0]          next_state   ;// combo part of FSM
 
 
+ always @(posedge clk) begin
+       state <= next_state;  
+ end
 
 
-    assign next_state = fsm_function(state, req_0, req_1);
-
-
-    function [SIZE-1:0] fsm_function;
-
-    input  [SIZE-1:0]  state ;
-    input    req_0 ;
-    input    req_1 ;
-
-
-
-    case(state)
+     case(state) // req_0 debe ir pegado a reset global
      ST_STOP : if (req_0 == 1'b1) begin  /* ADMA2 stays in this state in following cases:
                                          (1) After Power on reset or software reset.
                                          (2) All descriptor data transfers are completed
