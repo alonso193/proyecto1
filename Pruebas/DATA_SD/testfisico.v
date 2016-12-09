f920843cab108c48130490cf652414fa785e2796
Mario Castresana Avendaño <Mario Castresana Avendao>
2016-12-08 21:05:13 -0600

    agregando pruebas de capa física

---
+++ Pruebas/DATA_SD/testfisico.v
@@ -0,0 +1,40 @@
+//-----------------------------------------------------
+// PROYECTO 1  : SD HOST
+// Archivo     : testfisico.v
+// Descripcion : testbench para capa física de bloque de DATA
+// Estudiante  : Mario Castresana Avendaño - A41267
+//-----------------------------------------------------
+
+`timescale 1ns / 1ps
+
+//`include "DATA.v"
+`include "probador.v"
+`include "DATA_PHYSICAL.v"
+
+
+
+
+module testbench;
+
+
+// Wires para probar data
+
+
+//salidas de capa física
+ 
+      
+//probador para bloque de control de datos
+probador DataTest(
+
+
+);
+
+// Instanciar Unit Under Test (UUT) DATA
+DATA DATA_CONTROL(
+	//inputs
+
+	//outputs
+      
+);
+
+endmodule
