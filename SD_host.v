7aba1af88f9340702d2da68ce25969c644e6217e
Luis Mora <lmoramora94@gmail.com>
2016-12-08 21:30:45 -0600

    Pega Final

--- SD_host.v
+++ SD_host.v
@@ -1,22 +1,51 @@
 `include "REG.v"
 `include "CMD.v"
 `include "BloqueDATA.v"
+`include "modules/DMA.v"
 
-module SD_host (clk_host, reset_host, io_enable_cmd, CMD_PIN_IN, CMD_PIN_OUT, clk_SD, data_in_register, data_out_register, req_register, ack_register, addres_register, rw_register, DATA_PIN_IN, DATA_PIN_OUT, IO_enable_Phy_SD_CARD, pad_enable_Phy_PAD, pad_state_Phy_PAD);
+
+module SD_host (clk_host,
+				reset_host,
+				io_enable_cmd,
+				CMD_PIN_IN,
+				CMD_PIN_OUT,
+				clk_SD,
+				data_in_register,
+				data_out_register,
+				req_register,
+				ack_register,
+				addres_register,
+				rw_register,
+				DATA_PIN_IN,
+				DATA_PIN_OUT,
+				IO_enable_Phy_SD_CARD,
+				pad_enable_Phy_PAD,
+				pad_state_Phy_PAD,
+				cmd_reg_write,
+				cmd_reg_read,
+				Continue,
+				descriptor_table,
+				TRAN,
+				STOP);
+
     //señales del host
+
     input wire clk_host, reset_host;
     input wire DATA_PIN_IN;
     output wire DATA_PIN_OUT;
     output wire IO_enable_Phy_SD_CARD;
+
     //outputs para el estado del PAD
     output wire pad_enable_Phy_PAD;
     output wire pad_state_Phy_PAD;
 
     //señales del CMD
+
     input wire CMD_PIN_IN;
     output wire io_enable_cmd, CMD_PIN_OUT;
 
     //señales del register
+
     input wire [31:0]data_in_register;
     output wire [31:0] data_out_register;
     input wire req_register;
@@ -24,10 +53,20 @@ module SD_host (clk_host, reset_host, io_enable_cmd, CMD_PIN_IN, CMD_PIN_OUT, cl
     input wire [4:0]addres_register;
     input wire rw_register;
 
+	// señales de DMA
+	input wire cmd_reg_write;
+	input wire cmd_reg_read;
+	input wire Continue;
+	input wire [6:0][85:0] descriptor_table;
+	input wire TRAN;
+	input wire STOP;
+
     //señales compartidas del SD
+
     input wire clk_SD;
 
     //cables de conexion entre DMA y CMD
+
     wire new_command;
 
     //cables de conexion entre REG y CMD
@@ -47,20 +86,65 @@ module SD_host (clk_host, reset_host, io_enable_cmd, CMD_PIN_IN, CMD_PIN_OUT, cl
     wire multipleData_Regs_DATA;
     wire timeout_enable_Regs_DATA;
 
+	//wires entre DMA y DATA
+	wire   transfer_complete_DATA_DMA;
+
     assign timeout_Reg_Regs_DATA = bloque_registers.Timeout_Reg;
     assign writeRead_Regs_DATA = bloque_registers.writeRead;
     assign blockCount_Regs_DATA = bloque_registers.block_count;
     assign multipleData_Regs_DATA = bloque_registers.multipleData;
     assign timeout_enable_Regs_DATA = bloque_registers.timeout_enable;
 
-    //wire entre DMA y DATA
-    wire New_DAT_DMA_DATA;
-    wire transfer_complete_DATA_DMA;
-    
+    // wire entre DMA y FIFO
+
+	wire FIFO_EMPTY_DMA;
+	wire FIFO_FULL_DMA;
+	wire [63:0] system_address;
+	wire writing_mem; // 1 if dir = 0 and TFC = 0
+	wire reading_mem; //
+	wire reading_fifo;
+	wire writing_fifo;
+	wire [5:0] next_descriptor_index;
+	reg [5:0] descriptor_index;
+
+	always @(posedge clk_host) begin
+		descriptor_index <= next_descriptor_index;
+	end
+
+
+
+	DMA bloqueDMA(reset,
+				  clk_host,
+				  transfer_complete_DATA_DMA,
+				  cmd_reg_write,
+				  cmd_reg_read,
+				  Continue,
+				  TRAN,
+				  STOP,
+				  FIFO_EMPTY_DMA,
+				  FIFO_FULL_DMA,
+				  descriptor_table [0][85:22], // data_address
+				  descriptor_table [0][20:5], // length
+				  descriptor_index,
+				  descriptor_table [0][4], //act1
+				  descriptor_table [0][3], //act2
+				  descriptor_table [0][2], // END
+				  descriptor_table [0][1], // valid
+				  descriptor_table [0][0], // dir
+				  new_command, // To CMD block
+				  system_address,
+				  writing_mem, // 1 if dir = 0 and TFC = 0
+	              reading_mem, //
+	              reading_fifo,
+	              writing_fifo,
+	   		      next_descriptor_index);
+
+
+
 
     CMD bloque_CMD(clk_host, reset_host, new_command, cmd_argument, cmd_index, cmd_complete, cmd_index_error, response, CMD_PIN_OUT, IO_enable_pin, CMD_PIN_IN, clk_SD);
     REG bloque_registers(clk_host, rw_register, addres_register, data_in_register, data_out_register);
-    
+
     //Bloque de DATA
     BloqueDATA Data_Control(
     .CLK(clk_host),
@@ -73,7 +157,7 @@ module SD_host (clk_host, reset_host, io_enable_cmd, CMD_PIN_IN, CMD_PIN_OUT, cl
     .timeout_enable_Regs_DATA(timeout_enable_Regs_DATA),
     //.FIFO_OK_FIFO_DATA(),
     //.[31:0] dataFromFIFO_FIFO_Phy(),
-    .New_DAT_DMA_DATA(New_DAT_DMA_DATA),
+    //.New_DAT_DMA_DATA(),
     //.DATA_PIN_IN(DATA_PIN_IN),
     //.writeFIFO_enable_Phy_FIFO(),
     //.readFIFO_enable_Phy_FIFO(),
--- modules/DMA.v
+++ modules/DMA.v
@@ -1,98 +1,90 @@
 // *****************************
-// Luis Fernando Mora
+// Luis F. Mora
 // B24449
 // Circuitos Digitales II
-// Prof Enrique Coen
+// Prof. Enrique Coen
 // *****************************
 
-module DMA(input reset,                 
-           input clk, 
+module DMA(input reset,
+           input clk,
            input TFC, // Senal del fifo (cuando system_address = data_address + length)
            input cmd_reg_write, // Senal de inicio
-           input cmd_reg_read, // Senal de inicio 
+           input cmd_reg_read, // Senal de inicio
            input Continue, // Senal de reanudacion
            input TRAN, // Parte del descriptor address
            input STOP, // Interrupt signal
            input empty, // fifo empty
            input full, // fifo full
-           input [63:0] descriptor_index, // Posicion del descriptor address
            input [63:0] data_address, // Posicion donde empieza el dato (Address Field del Address Descriptor)
            input [15:0] length, // Tamano del dato en bytes (Length del Address Descriptor)
-           input [5:0] Attribute, // Siguiente codificacion MSB to LSB:  |Act2|Act1|END|valid|dir|
-           output start_transfer, 
-           output system_address, // 4 bytes currently being transfered
+		   input [5:0] descriptor_index, // Descriptor table de 64 entradas -> 6 bits de indexacion
+		   input  act1, // Attribute[4]
+		   input  act2, // Attribute[3]
+		   input  END, // Attribute [2]
+		   input  valid, // Attribute[1]
+		   input  dir, // Attribute[0] 1: from memory to SD / 0: from SD to memory
+           output start_transfer,
+           output [63:0] system_address, // 4 bytes currently being transfered
            output writing_mem, // 1 if dir = 0 and TFC = 0
-           output reading_mem, // 
+           output reading_mem, //
            output reading_fifo,
-           output writing_fifo); 
- 
- parameter ST_STOP  = 2'b00, 
+           output writing_fifo,
+		   output [5:0] next_descriptor_index);
+
+ parameter ST_STOP  = 2'b00,
            ST_FDS   = 2'b01,
            ST_CADR  = 2'b10,
-           ST_TFR   = 2'b11; 
- 
+           ST_TFR   = 2'b11;
+
 reg   [1:0]          state;          // Estado presente
-           
-reg                  reading_fifo,
-                     writing_fifo,                      
-                     reading_mem,
-                     writing_mem     // 1 if dir = 0 and TFC = 0
-                     start_transfer,
-                     act1,
-                     act2,
-                     END,
-                     valid,
-                     dir; 
 
-wire  [1:0]          next_state   ;// Proximo estado          
-             
+reg                  reading_fifo;
+reg                  writing_fifo;
+reg                  reading_mem;
+reg                  writing_mem;    // 1 if dir = 0 and TFC = 0
+reg                  start_transfer;
 
-reg [63:0]            system_address,  // Address of the 4 bytes currently being transfered
-                               
+
+reg  [1:0]          next_state; // Proximo estado
+reg  [63:0]         system_address; //
+reg  [5:0]			next_descriptor_index;
 
 
  // Asignar proximo estado
-           
- always @(posedge clk) begin
-       state <= next_state;  
- end
-         
- // Asignar variables de Attribute 
-           
-always @(*) begin
-                      assign  act2 = Attribute[4];
-                      assign  act1 = Attribute[3];
-                      assign  END = Attribute [2];
-                      assign  valid = Attribute[1];
-                      assign  dir = Attribute[0];
-end
-           
+
+ always  @ (posedge clk)
+  begin
+  state <= next_state;
+  end
+
  // Logica de transicion de estados
-           
+
 always @(*) begin
-   case(state) begin 
-ST_STOP : begin                                                          // ADMA2 stays in this state in following cases:                          
+
+   case(state)
+
+	ST_STOP : begin                                                      // ADMA2 stays in this state in following cases:
             if(reset) next_state = ST_STOP;                              // After reset
                else if(cmd_reg_write || Continue) next_state = ST_FDS;   // All descriptor data transfers are completed
           end                                                            // If a new DMA operation is started by writing Command register, go to
-                                                                         //  ST_FDS state. 
-              
-     ST_FDS :   begin                                    // DMA fetches a descriptor line: if (!Act2&&!Act1) NOP: go to next 
-                    if(!valid) next_state = ST_FDS;      // descriptor line. If descriptor line valid = 0 to go next descriptor line 
-                    else next_state = ST_CADR;           // Next descriptor line = descriptor_index      
+                                                                         //  ST_FDS state.
+
+     ST_FDS :   begin                                    // DMA fetches a descriptor line: if (!Act2&&!Act1) NOP: go to next
+                    if(!valid) next_state = ST_FDS;      // descriptor line. If descriptor line valid = 0 to go next descriptor line
+                    else next_state = ST_CADR;           // Next descriptor line = descriptor_index
                 end
-     ST_CADR :  begin                                             // Link operation loads another Descriptor address to ADMA System Address                                                                                 
+     ST_CADR :  begin                                             // Link operation loads another Descriptor address to ADMA System Address
                 if(TRAN) next_state = ST_TFR;                // register. In other operations, ADMA System Address register is
                        else next_state = ST_CADR ;
                 if(!END&&!TRAN) next_state = ST_FDS  ;      // incremented to point next descriptor line. If TRAN=0, go to ST_TFR state.
                 end                                               //  data_address = system_address
-                           
-ST_TFR : begin                                                           // Data transfer of one descriptor line is executed between system memory                                       
-                      if(!TFC) next_state = ST_TFR;                     // and SD card. If data transfer continues (End=0) go to ST_TFR. goto_address++        
-                      else if(TFC&&(END||STOP)) next_state = ST_STOP;    // If data transfer is complete (End = 1) got to ST_STOP. 
-                       else if(TFC(!END||!STOP)) next_state = ST_FDS;    // Stop signal is generated through interrupts 
-              end 
-                
+
+ST_TFR : begin                                                            // Data transfer of one descriptor line is executed between system memory
+                      if(!TFC) next_state = ST_TFR;                       // and SD card. If data transfer continues (End=0) go to ST_TFR. goto_address++
+                      else if(TFC&&(END||STOP)) next_state = ST_STOP;     // If data transfer is complete (End = 1) got to ST_STOP.
+                       else if(TFC&&(!END||!STOP)) next_state = ST_FDS;   // Stop signal is generated through interrupts
+              end
+
     default : next_state = ST_STOP;
     endcase
 end // always(*)
@@ -100,47 +92,56 @@ end // always(*)
 // Logica de las salidas
 
 always @(*) begin
-           case(state) begin
+           case(state)
                       ST_STOP: begin
                                start_transfer = 0;
-                               system_address = 0; // 4 bytes currently being transfered
+                               system_address = data_address; // 4 bytes currently being transfered
                                writing_mem  = 0;   // 1 if dir = 0 and TFC = 0
-                               reading_mem  = 0;   // 
-                               reading_fifo = 0; 
-                               writing_fifo = 0; 
+                               reading_mem  = 0;   //
+                               reading_fifo = 0;
+                               writing_fifo = 0;
                       end
-                      
+
                       ST_FDS: begin
-                                 
-                               start_transfer = 0; 
+
+                               start_transfer = 0;
                                writing_mem =  0 ;   // 1 if dir = 0 and TFC = 0
-                               reading_mem  = 0;   // 
+                               reading_mem  = 0;   //
                                reading_fifo = 0;
-                               writing_fifo = 0; 
-                               if(!valid) descriptor_index = descriptor_index + 1; 
+                               writing_fifo = 0;
+                               if(!valid) next_descriptor_index = descriptor_index + 1;
                       end
-                      
+
                       ST_CADR: begin
-                               start_transfer = 0; 
-                               system_address  = data_address + 1; // 4 bytes currently being transfered
+                               start_transfer = 0;
+                	           system_address  = data_address + 1; // 4 bytes currently being transfered
                                writing_mem  = 0;    // 1 if dir = 0 and TFC = 0
-                               reading_mem  = 0;   // 
+                               reading_mem  = 0;   //
                                reading_fifo = 0;
-                               writing_fifo = 0; 
+                               writing_fifo = 0;
                       end
                       ST_TFR: begin
-                               start_transfer = 1; 
+                               start_transfer = 1;
                                system_address =  system_address; // 4 bytes currently being transfered
-                               writing_mem = !dir    // 1 if dir = 0 and TFC = 0
-                               reading_mem = dir;    // 
+                               writing_mem = !dir;    // 1 if dir = 0 and TFC = 0
+                               reading_mem = dir;    //
                                reading_fifo = !dir; // FIXME no lo tengo muy claro
                                writing_fifo = dir;  // FIXME no lo tengo muy claro
-                      end                
-                      
+                      end
+
+					  default: begin
+					  start_transfer = 0;
+					  system_address = data_address; // 4 bytes currently being transfered
+					  writing_mem  = 0;   // 1 if dir = 0 and TFC = 0
+					  reading_mem  = 0;   //
+					  reading_fifo = 0;
+					  writing_fifo = 0;
+			 end
+
            endcase
-           
+
 end // always @(*)
-                    
+
 
 
 endmodule
